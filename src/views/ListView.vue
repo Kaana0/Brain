c878f31d6430c52275f580e81f1c939a8676ead5
kaana0 <k.swechocc@gmail.com>
2022-04-24 23:31:17 +0900

    commit

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -4,7 +4,7 @@
       <li><router-link to="/goodbrain">頭に良い</router-link></li>
       <li><router-link to="/focus">集中力Up</router-link></li>
       <li><router-link to="/memory">記憶力Up</router-link></li>
-      <li><router-link to="/thinking">思考力Up</router-link></li>
+      <li><router-link to="/thinking"><img src="../assets/denki.svg" alt=""> 思考力Up</router-link></li>
       <li><router-link to="/brainup">脳の活性化</router-link></li>
       <li><router-link to="/season">季節別</router-link></li>
       <li><router-link to="/vegetablelist">食材別</router-link></li>
