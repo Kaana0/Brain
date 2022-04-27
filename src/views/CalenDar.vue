41b12b8b02fcceccfe2ef5d41015a2673641906a
kaana0 <k.swechocc@gmail.com>
2022-04-27 12:51:16 +0900

    commit

--- src/views/CalenDar.vue
+++ src/views/CalenDar.vue
@@ -1,6 +1,6 @@
 <template>
   <div id='app'>
-    <v-menu v-model="menu">
+    <!-- <v-menu v-model="menu">
       <template v-slot:activator="{ on, attrs }">
         <v-text-field v-model="text" v-bind="attrs" v-on="on" readonly clearable>
         </v-text-field>
@@ -8,7 +8,8 @@
       <v-date-picker v-model="picker" @input="formatDate(picker)">
       </v-date-picker>
     </v-menu>
-    {{ picker }}         
+    {{ picker }} -->
+        <v-calendar :attributes="attributes" @dayclick="onDayClick"></v-calendar>
   <FooterComp></FooterComp>
   </div>
 </template>
@@ -22,9 +23,31 @@ export default {
       menu: "",
       text: "",
       picker: "",
+
+      attributes: [
+        {highlight: {color:'blue', fillMode: 'solid',},
+        dates: ['2022-4-15'],
+        popover: {
+          label: 'アドカレ担当日',
+        },
+    },
+  ],
+
     };
   },
   methods: {
+    onDayClick(day) {
+      console.log(day.id)
+      // this.attributes.push(
+      //   {
+      //     highlight: {color:'blue', fillMode: 'solid',},
+      //     dates: [day.id],
+      //   popover: {
+      //     label: '寝る',
+      //   },
+      //   })
+      this.$router.push()
+    },
     formatDate(date) {
       if (!date) return null;
       const [year, month, day] = date.split("-");
