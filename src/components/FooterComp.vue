97d565bad6648b3a35844bb0e52246763272f743
kaana0 <k.swechocc@gmail.com>
2022-05-15 22:47:24 +0900

    commit

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="footer">
       <ul class="footer">
-        <router-link to="/search" class="li">探す</router-link>
-        <router-link to="/list" class="li">一覧</router-link>
-        <router-link to="/calendar" class="li">日付</router-link>
+        <router-link class="button li" to="/search">探す</router-link>
+        <router-link class="button li" to="/list">一覧</router-link>
+        <router-link class="button li" to="/calendar">日付</router-link>
         <!-- <router-link to="/okiniiri" class="li">お気に入り</router-link> -->
       </ul>
   </div>
@@ -13,13 +13,37 @@
 </script>
 <style scoped>
 .li {
-  border: solid 1px rgb(85, 50, 3);
   width: 100%;
   margin: 0;
-  /* height: 4em; */
   line-height: 4;
 }
-ul {
-  background-color: mistyrose;
+.button {
+    background: mistyrose;
+    border-radius: 3px;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+    max-width: 220px;
+    padding: 10px 25px;
+    color: rgb(85, 50, 3);
+    transition: 0.3s ease-in-out;
+    font-weight: 600;
+    box-shadow: 5px 5px 0 #903a38;
+    border-radius: 50px;
+}
+.button:hover {
+  background-color: #c49494;
+  box-shadow: 0 0 0;
+  transform: translate(5px, 5px);
+}
+.button:after {
+    position: absolute;
+    top: 50%;
+    right: 20px;
+    transition: 0.2s ease-in-out;
+    font-weight: 900;
+    transform: translateY(-50%);
 }
 </style>
