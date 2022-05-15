d2949164f36fabf09d9fa8e61dda4494e2c69cc5
kaana0 <k.swechocc@gmail.com>
2022-05-15 20:47:21 +0900

    commit

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -6,8 +6,27 @@
 
 <style scoped>
 .li {
+  position: relative;
+  overflow: hidden;
+}
+.li:after {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 1px;
+  background-color: rgb(70, 2, 2);
+  transform: translate(-100%, 0);
+  transition: transform cubic-bezier(0.215, 0.61, 0.355, 1) 0.4s;
+  content: "";
+}
+.li:hover:after {
+  transform: translate(0, 0);
+}
+.li {
   display: flex;
   justify-content: flex-end;
   padding-right: 7%;
+  width: 100px;
 }
 </style>
