15ab2fcaf0c4ddc772c13d7c3ff2b450bce3f16e
kaana0 <k.swechocc@gmail.com>
2022-05-15 20:02:58 +0900

    commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,8 +2,8 @@
   <div id="app">
       <div class="about">
       <ClockComp></ClockComp>
-      <h1>How to Use...</h1>
-      <h3>Great Brainを手に入れよう！</h3>
+      <h1 class="text__slideIn">How to Use...</h1>
+      <h3 class="text__slideIn">Great Brainを<br>手に入れよう！</h3>
       <p>普段食べているものが<br>どんな成分かを知って、<br>頭に良い食事をしましょう。</p>
       <p>記憶力、集中力、<br>脳に良い食べ物を取り入れて<br><span>Happy</span>な日々になりますように。</p>
       </div><!-- /.about -->
@@ -21,6 +21,36 @@ export default {
 }
 </script>
 <style scoped>
+.text__slideIn {
+  animation: slideIn 1.6s infinite;
+  animation-iteration-count: 1;
+  font-size: 40px;
+}
+p {
+  animation: slideIn2 1.6s infinite;
+  animation-iteration-count: 1;
+  font-size: 20px;
+}
+@keyframes slideIn {
+  0% {
+    opacity: 0;
+    transform: translateX(100px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+@keyframes slideIn2 {
+  0% {
+    opacity: 0;
+    transform: translateX(-100px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
 h1 {
   padding: 10px;
 }
@@ -28,7 +58,7 @@ h3 {
   padding-bottom: 10px;
 }
 p {
-  line-height: 3;
+  line-height: 2.5;
 }
 span {
   color: gold;
