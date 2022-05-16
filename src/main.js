c8c4ee0d9441e712e5974b981e9464f138a3b7e3
kaana0 <k.swechocc@gmail.com>
2022-05-16 15:12:52 +0900

    commit

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VCalendar from 'v-calendar'
+import './registerServiceWorker'
 
-Vue.use(VCalendar) 
+Vue.use(VCalendar)
 Vue.config.productionTip = false
 
 new Vue({
