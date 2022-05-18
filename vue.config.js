3d50ff8432e91b01245c56bbe3d75f7c84a9c46c
kaana0 <k.swechocc@gmail.com>
2022-05-18 10:49:25 +0900

    commit

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // pwa: {
-  //   appleMobileWebAppCapable: 'yes',
-  //   appleMobileWebAppStatusBarStyle: 'black',
-  //   iconPaths: {
-  //     favicon32: 'imgs/icons/favicon-32x32.png',
-  //     favicon16: 'imgs/icons/favicon-16x16.png',
-  //     appleTouchIcon: 'imgs/icons/apple-touch-icon-precomposed.png',
-  //     maskIcon: 'imgs/icons/site-tile-310x310.png',
-  //     msTileImage: 'imgs/icons/site-tile-310x310.png'
-  //   }
-  // }
+  pwa: {
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      favicon32: 'imgs/icons/favicon-32x32.png',
+      favicon16: 'imgs/icons/favicon-16x16.png',
+      appleTouchIcon: 'imgs/icons/apple-touch-icon-precomposed.png',
+      maskIcon: 'imgs/icons/site-tile-310x310.png',
+      msTileImage: 'imgs/icons/site-tile-310x310.png'
+    }
+  }
 })
 
