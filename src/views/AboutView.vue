c6ba821885785052b2b6c89a16eff3ac03164a99
kaana0 <k.swechocc@gmail.com>
2022-05-20 00:41:17 +0900

    commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,8 +5,22 @@
       <h1 class="text__slideIn">How to Use...</h1>
       <h3 class="text__slideIn">Great Brainを<br>手に入れよう！</h3>
       <p>普段食べているものが<br>どんな成分かを知って、<br>頭に良い食事をしましょう。</p>
-      <p>記憶力、集中力、<br>脳に良い食べ物を取り入れて<br><span>Happy</span>な日々になりますように。</p>
+      <p>記憶力、集中力、<br>脳に良い食べ物を取り入れて<br><span class="span">Happy</span>な日々になりますように。</p>
       </div><!-- /.about -->
+      <div class="nav">
+      <div class="hide" @click="isHide = true" v-show="!isHide">
+      <p class="setumei">食材を<br>入力できるよ!</p>
+      <img src="../assets/yubi1.svg" alt="">
+      </div>
+      <div class="hide"  @click="isHide1 = true" v-show="!isHide1">
+      <p class="setumei">項目別に<br>なってるよ！</p>
+      <img src="../assets/yubi1.svg" alt="">
+      </div>
+      <div class="hide" @click="isHide2 = true" v-show="!isHide2">
+      <p class="setumei">その日食べた物を<br>押してね！</p>
+      <img src="../assets/yubi1.svg" alt="">
+      </div>
+      </div>
       <FooterComp></FooterComp>
   </div>
 </template>
@@ -14,6 +28,13 @@
 import ClockComp from '@/components/ClockComp.vue';
 import FooterComp from '@/components/FooterComp.vue';
 export default {
+  data() {
+    return {
+      isHide: false,
+      isHide1: false,
+      isHide2: false
+    }
+  },
   components: {
     ClockComp,
     FooterComp,
@@ -21,7 +42,20 @@ export default {
 }
 </script>
 <style scoped>
-
+.hide {
+  /* background-color: rgba(red, green, rgb(231, 231, 241), alpha); */
+}
+.nav {
+  display: flex;
+  position: fixed;
+  bottom: 0;
+  z-index: 1;
+  padding-bottom: 50px;
+}
+.setumei {
+  font-size: 5px;
+  color: #FFF;
+}
 p {
   animation: slideIn2 1.6s infinite;
   animation-iteration-count: 1;
@@ -46,7 +80,7 @@ h3 {
 p {
   line-height: 2.5;
 }
-span {
+.span {
   color: gold;
 }
 </style>
