57570fb898a4d0a2b8bd96c54e637708240eb3d3
kaana0 <k.swechocc@gmail.com>
2022-05-25 14:41:33 +0900

    0525

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <HeaderComp></HeaderComp>
     <h1 class="text__bound">Great</h1>
     <h1 class="text__bound">Brain !!</h1>
+  <router-link to="/about" class="li">
   <img class="text__bound" src="../../public/nou.svg" alt="">
+  </router-link>
   </div>
 </template>
 
 <script>
-import HeaderComp from '@/components/HeaderComp.vue';
 
 export default {
   components: {
-    HeaderComp,
   }
 }
 </script>
